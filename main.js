ec5b66f6dbe1e1991c9d0411573d6c4225c9e7f1
MikaCinc <mihajlo.ls00@outlook.com>
2018-09-02 01:18:38 +0200

    Little changes with text

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ const navigation = (index) => {
     var btns = document.getElementById("navigation").getElementsByTagName("button");
     for (let i = 0; i < btns.length; i++) {
         if (i === index) {
-            btns[i].className = "btn btn-secondary"
+            btns[i].className = "btn btn-success"
         }
     }
 }
