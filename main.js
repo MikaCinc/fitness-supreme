f978e654dae35628cdffc8b8cf3b64e77228ef17
MikaCinc <mihajlo.ls00@outlook.com>
2018-09-01 18:00:04 +0200

    first real changes, all cards finished

--- main.js
+++ main.js
@@ -4,13 +4,65 @@ window.onload = () => {
 
 var State = {
     Gender: "",
-    
+    Age: "",
 }
 
 const initial = () => {
-    
+    showCard(0);
+    navigation(0);
 }
 
-const showCard = () => {
+const showCard = (index) => {
+    var cards = document.getElementsByClassName("card");
+    for (let i = 0; i < cards.length; i++) {
+        if (i === index) {
+            cards[i].className = "card w-50 mx-auto mt-3"
+        } else {
+            cards[i].className = "card w-50 mx-auto mt-3 d-none"
+        }
+    }
+}
 
+const navigation = (index) => {
+    var btns = document.getElementById("navigation").getElementsByTagName("button");
+    for (let i = 0; i < btns.length; i++) {
+        if (i === index) {
+            btns[i].className = "btn btn-secondary"
+        }
+    }
+}
+
+const gender = (type) => {
+    State.Gender = type
+    showCard(1);
+    navigation(1);
+}
+
+
+const changeMetrics = (type) => {
+    if(type === "metrics") {
+        $("#" + type).collapse('show');
+        $("#imperial").collapse('hide');
+    } else {
+        $("#" + type).collapse('show');
+        $("#metrics").collapse('hide');
+    }
+}
+
+const measurements = () => {
+    showCard(2);
+    navigation(2);
+    return false;
+}
+
+const age = (age) => {
+    State.Age = age;
+    showCard(3);
+    navigation(3);
+}
+
+const activity = (activity) => {
+    State.activity = activity;
+    showCard(4);
+    navigation(4);
 }
